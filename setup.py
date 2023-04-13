f1e7692af6408ed643c7edc32d79ccdfa5b6324a
Corentin Sallin <62545405+ThePotatoPvP@users.noreply.github.com>
2023-04-13 20:52:13 +0200

    Added icon

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-import sys
 from cx_Freeze import setup, Executable
 
-# informations sur l'exécutable
+
 exe = Executable(
     script="main.py",
     base="Win32GUI",
     icon="ressources/coesite.ico"
 )
 
-# création du setup
+
 setup(
     name="Coesite",
     version="0.1",
